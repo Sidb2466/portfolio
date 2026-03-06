5bd68ac567ef41824a2484e024131d0a6cf9cdb8
Sidb2466 <sidbrahms@gmail.com>
2026-03-06 20:46:37 +0530

    game.js

--- game.js
+++ game.js
@@ -1,278 +1,215 @@
-/*
-  This project is inspired by and partially based on
-  "Mario Bros Resume" by Miquel Camps
-  https://vivirenremoto.github.io/mariocv/
+body{overflow-x: hidden;
+  font-family: 'VT323', monospace;
+  font-size: 20px;
+  background: #6B88FF;}
 
-  Code has been modified, extended, and customized
-  for a personal portfolio project by Siddharth Brahmankar.
-*/
-var ismobile = navigator.userAgent.match(/(iPhone)|(iPod)|(android)|(webOS)|(BlackBerry)/i);
-var scroll_x = $(window).width() / 2;
-var floor_x = 0;
-var mario_x = 0;
-var direction = false;
-var music_play = false;
-var interval_left = false;
-var interval_right = false;
-var boxHitThisJump = false;
-var isJumping = false;
-var jumpInterval = null;
-const jumpSound = new Audio('public/jump.mp3');
-const boxHitSound = new Audio('public/box-hit.mp3');
-const WORLD_WIDTH = 3200;
-const flagSound = new Audio('public/flag.mp3');
-flagSound.volume = 0.4;
-boxHitSound.volume = 0.3;
-jumpSound.volume = 0.3;
+b, span{display: block;}
+b{margin-bottom: 15px;font-size:30px;}
 
-if (ismobile) scroll_x -= 170;
-else scroll_x -= 240;
-$('#scroll').css('left', scroll_x + 'px');
+#floor{position: absolute;
+  background: url('public/img/floor.png');
+  bottom: 0;
+  width: 100%;
+  left: 0;
+  height: 106px;}
 
-function renderMario(yOffset = 0) {
-    const scale = direction === 'left' ? -1 : 1;
-    const mario = document.getElementById('mario');
-    mario.style.transform = `translateY(${-yOffset}px) scaleX(${scale})`;
+#scroll{image-rendering: crisp-edges;
+  position: fixed;
+  top: 0;
+  bottom: 106px;
+  width: 3125px;
+  background: url('public/img/background.png');
+  background-position: left bottom;
+  background-repeat: no-repeat;
+  background-size: auto 100%;}
+  
+#mario{background-image: url('public/img/mario.png');
+  background-position: top left;
+  background-repeat: no-repeat;
+  position: fixed;
+  z-index: 9999;
+  bottom: 106px;
+  left: 50%;
+  margin-left: -30px;
+  width: 65px;
+  height: 129px;
+  background-blend-mode: color;}
+
+.box{position: relative;
+  min-height: 270px;
+  float: left;
+  background: #F7C400;               
+  border-radius: 6px; 
+  padding: 20px;
+  margin-left: 100px;
+  top: 50%;
+  margin-top: -200px;
+  width: 300px;
+  border-radius: 12px;
+  overflow: hidden;
+  box-shadow:  inset -4px -4px 0 rgba(0,0,0,0.18),inset 4px 4px 0 rgba(255,255,255,0.28);}
+
+#btn_left,
+#btn_right,
+#btn_up{position: fixed;
+  width: 64px;
+  height: 64px;
+  font-size: 48px;
+  background: #ff0000;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  z-index: 10000;
+  user-select: none;
+  box-shadow:  inset -3px -3px 0 rgba(0,0,0,0.18),inset 4px 4px 0 rgba(255,255,255,0.28);}
+
+#btn_left svg,
+#btn_right svg,
+#btn_up svg{width: 50px;
+  height: 50px;
+  fill: rgb(255, 255, 255);}
+
+#btn_left{left: 20px;
+  bottom: 20px;}
+
+#btn_right{left: 100px;
+  bottom: 20px;}
+
+#btn_up{left: 60px;     
+  bottom: 100px;}
+
+#btn_left:active,
+#btn_right:active,
+#btn_up:active{transform: translateY(2px);
+  background: #000;}
+
+#music-btn{position: fixed;
+  top: 15px;
+  right: 20px;
+  z-index: 10000;
+  font-family: 'VT323',monospace;
+  font-size: 22px;
+  background: orange;
+  color: white;
+  border: none;
+  padding: 8px 14px;
+  cursor: pointer;
+  border-radius: 8px;
+  box-shadow:  inset -4px -4px 0 rgba(0,0,0,0.18),inset 4px 4px 0 rgba(255,255,255,0.28);}
+
+#music-btn {outline: none;}
+
+@keyframes box-bounce {
+  0%   { top: 50%; }
+  30%  { top: calc(50% - 20px); }
+  100% { top: 50%; }}
+
+.box.hit{animation: box-bounce 0.25s ease-out;}
+
+#end-pole{position: absolute;
+  bottom: 0px;
+  left:3300px; 
+  width: 40px;
+  height: 560px;
+  z-index: 200;}
+
+#end-pole .pole{position: absolute;
+  left: 50%;
+  transform: translateX(-100%);
+  width: 12px;
+  height: 100%;
+  background: #eee;
+  border-radius: 3px;
+    box-shadow:
+    inset -5px -5px 0 rgba(0,0,0,0.2),
+    inset 5px 5px 0 rgba(255,255,255,0.4);}
+
+#end-pole .flag{position: absolute;
+  top: 0;
+  left: 14px;
+  width: 0;
+  height: 0;
+  background: none; 
+  border-top: 0px solid transparent;
+  border-bottom: 80px solid transparent;
+  border-left: 120px solid #e60000;
+  transition: top 0.6s ease;}
+
+#end-pole .pole-cap{position: absolute;
+  top: -15px;                
+  left: 36%;
+  transform: translateX(-50%);
+  width: 20px;
+  height: 20px;
+  background: #eee;
+  border-radius: 50%;
+  box-shadow:
+    inset -2px -2px 0 rgba(0,0,0,0.2),
+    inset 2px 2px 0 rgba(255,255,255,0.4);}
+
+.hidden {
+  display: none;
 }
 
-function moveTo(pos) {
-    let diff = ismobile ? 14 : 22;
-    checkPoleHit();
-    if (pos === 'left') {
-        floor_x += diff;
-        scroll_x += diff;
-        mario_x -= 65;
-        if (mario_x === -195) mario_x = 0;
-    } 
-    else if (pos === 'right') {
-        floor_x -= diff;
-        scroll_x -= diff;
-        mario_x -= 65;
-        if (mario_x === -195) mario_x = 0;
-    }
-    if (scroll_x < -WORLD_WIDTH) {
-    scroll_x = $(window).width();
-    }
-    else if (scroll_x > $(window).width()) {
-    scroll_x = -WORLD_WIDTH;
-    }
-    $('#scroll').css('left', scroll_x + 'px');
-    $('#floor').css('background-position-x', floor_x + 'px');
-    $('#mario').css('background-position-x', mario_x + 'px');
+.box-placeholder {
+  text-align: center;
+  font-size: 28px;
+  opacity: 0.8;
 }
 
-function playMusic() {
-    const music = document.getElementById("bg_music");
-    if (!music_play) {
-        music.volume = 0.4;
-        music.play();
-        music_play = true;
-        $('#music-btn').text('Music: ON');
-    }
+.box.revealed .box-placeholder {
+  display: none;
 }
 
-function moveLeft() {
-    playMusic();
-    if (!interval_left) {
-        interval_left = setInterval(function () {
-            moveTo('left');
-        }, 60);
-    }
+.box.revealed .box-content {
+  display: block;
 }
 
-function moveRight() {
-    playMusic();
-    if (!interval_right) {
-        interval_right = setInterval(function () {
-            moveTo('right');
-        }, 60);
-    }
+.box {
+  position: relative;
 }
 
-function stopMove() {
-    clearInterval(interval_left);
-    clearInterval(interval_right);
-    interval_left = false;
-    interval_right = false;
+.box-placeholder {
+  position: absolute;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+  transition: transform 0.4s ease;
+  font-size: 55px;
+  color: #000;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  pointer-events: none;
+  transition: transform 0.4s ease; 
 }
 
-function hitBoxesDuringJump(marioRect) {
-    if (boxHitThisJump) return;
-    document.querySelectorAll('.box').forEach(box => {
-        const boxRect = box.getBoundingClientRect();
-        const horizontalHit =
-            marioRect.right > boxRect.left + 10 &&
-            marioRect.left < boxRect.right - 10;
-        const verticalHit =
-            marioRect.top <= boxRect.bottom + 5 &&
-            marioRect.top >= boxRect.bottom - 30;
-        if (horizontalHit && verticalHit) {
-            boxHitThisJump = true; 
-            box.classList.add('hit');
-            boxHitSound.currentTime = 0;
-            boxHitSound.play();
-            setTimeout(() => {
-                box.classList.remove('hit');
-            }, 250);
-            if (box.classList.contains('revealed')) return;
-            box.classList.add('revealed');
-        }
-    });
+@media (max-height: 500px) and (orientation: landscape) {
+    .box {
+        margin-top: -100px !important;
+        width: 140px !important;
+        min-height: 80px !important;
+        padding: 8px !important;
+    }
+
+    b {
+        font-size: 14px !important;
+        margin-bottom: 4px !important;
+    }
+
+    .box span,
+    .box-content span {
+        font-size: 11px !important;
+    }
+
+    #floor {
+        height: 70px !important;
+    }
+
+    #mario {
+        bottom: 70px !important;
+        width: 35px !important;
+        height: 70px !important;
+    }
 }
-
-function jump() {
-    if (isJumping) return;
-    isJumping = true;
-    boxHitThisJump = false;
-    jumpSound.currentTime = 0;
-    jumpSound.play();
-    let velocity = window.innerHeight < 700 ? 18 : 14;
-    let gravity = 1;
-    let position = 0;
-    let maxJump = Infinity;
-    const mario = document.getElementById('mario');
-    const marioRect = mario.getBoundingClientRect();
-    document.querySelectorAll('.box').forEach(box => {
-        const boxRect = box.getBoundingClientRect();
-        const horizontalHit =
-            marioRect.right > boxRect.left &&
-            marioRect.left < boxRect.right;
-        if (horizontalHit && boxRect.bottom < marioRect.top) {
-            const distance = marioRect.top - boxRect.bottom;
-            maxJump = Math.min(maxJump, distance);
-        }
-    });
-    jumpInterval = setInterval(() => {
-        velocity -= gravity;
-        position += velocity;
-        if (position > maxJump) {
-            position = maxJump;
-            velocity = 0;
-        }
-        if (position <= 0 && velocity < 0) {
-            position = 0;
-            clearInterval(jumpInterval);
-            jumpInterval = null;
-            isJumping = false;
-        }
-        const marioRect = mario.getBoundingClientRect();
-        hitBoxesDuringJump(marioRect);
-        renderMario(position);
-    }, 21);
-}
-
-function checkPoleHit() {
-  const pole = document.getElementById('end-pole');
-  if (!pole) return;
-  if (direction !== 'right') return;
-  const mario = document.getElementById('mario');
-  const m = mario.getBoundingClientRect();
-  const p = pole.getBoundingClientRect();
-  const hitFromRight =
-    m.left <= p.right &&    
-    m.right > p.left &&
-    m.bottom > p.top &&
-    m.top < p.bottom;
-  if (hitFromRight) finishLevel();
-}
-
-let flagActive = false;
-
-function finishLevel() {
-  if (flagActive) return;
-  flagActive = true;
-  flagSound.currentTime = 0;
-  flagSound.play();
-  const flag = document.querySelector('#end-pole .flag');
-  if (!flag) return;
-  flag.style.transition = 'top 0.6s';
-  flag.style.top = '400px';
-  setTimeout(() => {
-    flag.style.transition = 'none';
-    flag.style.top = '0px';
-    flagActive = false;
-  }, 1200);
-}
-
-function randomizeHitMePositions() {
-    document.querySelectorAll('.box:not(.revealed)').forEach(box => {
-        const placeholder = box.querySelector('.box-placeholder');
-        if (!placeholder) return;
-
-        const boxWidth = box.clientWidth;
-        const boxHeight = box.clientHeight;
-
-        const textWidth = placeholder.offsetWidth;
-        const textHeight = placeholder.offsetHeight;
-
-        const maxX = boxWidth - textWidth;
-        const maxY = boxHeight - textHeight;
-
-        const x = Math.random() * maxX;
-        const y = Math.random() * maxY;
-
-        placeholder.style.transform = `translate(${x}px, ${y}px)`;
-    });
-}
-
-$(function () {
-    setInterval(randomizeHitMePositions, 900);
-    $('body').attr('tabindex', '-1').focus();
-    $("body").keydown(function (e) {
-    if (e.key === 'ArrowLeft') {
-        direction = 'left';
-        renderMario(0);
-        moveLeft();
-    }
-    else if (e.key === 'ArrowRight') {
-        direction = 'right';
-        renderMario(0);
-        moveRight();
-    }
-    else if (e.key === ' ' || e.key === 'ArrowUp') {
-        jump();
-    }
-    });
-    $("body").keyup(function (e) {
-    if (e.keyCode == 37 || e.keyCode == 39) {
-        stopMove();
-    }
-    });
-    $('#btn_left').on('mousedown touchstart', function () {
-    direction = 'left';
-    renderMario(0);
-    moveLeft();
-    });
-    $('#btn_right').on('mousedown touchstart', function () {
-    direction = 'right';
-    renderMario(0);
-    moveRight();
-    });
-    $('#btn_up').on('mousedown touchstart', function () {
-    jump();
-    });
-    $('#btn_left, #btn_right').on('mouseup touchend', function () {
-        stopMove();
-    });
-    $('#music-btn').on('click', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const music = document.getElementById("bg_music");
-    if (music_play) {
-        music.pause();
-        music_play = false;
-        $(this).text('Music: OFF');
-    } else {
-        music.volume = 0.4;
-        music.play();
-        music_play = true;
-        $(this).text('Music: ON');
-    }
-    $(document).on('keydown', function (e) {
-    if (e.code === 'Space') {
-        e.preventDefault();
-    }
-    });
-    
-});
-});
